14e57293ccdcfb588e7ca9f3766db452b86a8044
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2025-03-26 17:58:02 +0100

    Implemented the first 3 steps of the algorithm. - computed `|*w_A*|` for each nonterminal *A* - checked for uniqual lengths for words - sorted nonterminals, descending by size of word

--- input.py
+++ input.py
@@ -1,9 +1,11 @@
+# Replace with your grammar. Every key is a nonterminal and its value is a list of symbols, which are either terminals or nonterminals.
 G = {
     "S": ["A", "B"], # S → AB
     "A": ["a", "C"], # 𝐴 → 𝑎C
     "C": ["b", "c"], # C → bc
     "B": ["d"] # 𝐵 → 𝑑
 }
+
 
 #Alternative compact array representation
 # G = {
